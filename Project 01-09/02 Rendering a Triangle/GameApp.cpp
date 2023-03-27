d5d2ad179c34a92e97ed4aae7d2f96ab856a7fa0
GatsbyChenJk <128161055+GatsbyChenJk@users.noreply.github.com>
2023-03-27 11:34:40 +0800

    Add files via upload

--- Project 01-09/02 Rendering a Triangle/GameApp.cpp
+++ Project 01-09/02 Rendering a Triangle/GameApp.cpp
@@ -78,8 +78,8 @@ bool GameApp::InitResource()
     // 设置三角形顶点
     VertexPosColor vertices[] =
     {
-        { XMFLOAT3(0.0f, 0.5f, 0.5f), XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f) },
-        { XMFLOAT3(0.5f, -0.5f, 0.5f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f) },
+        { XMFLOAT3(0.0f, 0.3f, 0.5f), XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f) },
+        { XMFLOAT3(0.1f, -0.5f, 0.5f), XMFLOAT4(1.0f, 0.0f, 1.0f, 1.0f) },
         { XMFLOAT3(-0.5f, -0.5f, 0.5f), XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f) }
     };
     // 设置顶点缓冲区描述
